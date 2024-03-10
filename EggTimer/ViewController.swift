f77c6f32ab56dc05789ccf9ee61bf9b8add9f81b
pravinrajpaul <pravinrajpaul@gmail.com>
2024-03-10 13:51:09 +0530

    Initial Checkin

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    let eggTimes: [String:Int] = ["Soft": 300, "Medium": 480, "Hard": 720]
+    var timer = Timer()
+    var storedHardness: String? = nil
+    var storedTime = 0
+    var hardnessTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func cookingLevelTapped(_ sender: UIButton) {
+        let hardness = (sender.titleLabel?.text)!
+        if (storedHardness != hardness || storedHardness == nil) {
+            timer.invalidate()
+            storedTime = 0
+            hardnessTime = eggTimes[hardness]!
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            storedHardness = hardness
+        }
+        else {return}
+    }
+    
+    @objc func updateTimer() {
+        if storedTime < hardnessTime {
+            storedTime += 1
+            progressBar.setProgress(Float(storedTime)/Float(hardnessTime), animated: true)
+        }
+        else {
+            timer.invalidate()
+            storedHardness = nil
+        }
+    }
 
 }
 
